183e638535301cd9b9f44e09161bfc92488cfef4
JTK222 <lukasporwit3@googlemail.com>
2018-05-27 01:15:21 +0200

    Added First Half of Networking Section

---
+++ src/main/java/net/dark_roleplay/library/networking/PacketBase.java
@@ -0,0 +1,76 @@
+package net.dark_roleplay.library.networking;
+
+import org.apache.logging.log4j.LogManager;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * Created: 26.05.2018
+ * Last edit: 26.05.2018
+ * Last edit by: JTK222
+ * Version added: 0.1.0
+ * State: completed
+ */
+public abstract class PacketBase<REQ extends IMessage> implements IMessage, IMessageHandler<REQ, REQ> {
+
+	@Override
+	public REQ onMessage(REQ message, MessageContext ctx) {
+		if(ctx.side == Side.SERVER) {
+			handleServerSide(message, ctx.getServerHandler().player);
+		} else {
+			handleClientSide(message, null);
+		}
+		return null;
+	}
+
+	/**
+	 * Called when an Instance of this Packet is received on the Client
+	 * @param message
+	 * @param player
+	 */
+	@SideOnly(Side.CLIENT)
+	public abstract void handleClientSide(REQ message, EntityPlayer player);
+	
+	/**
+	 * Called when an Instance of this Packet is received on the Server9
+	 * @param message
+	 * @param player
+	 */
+	public abstract void handleServerSide(REQ message, EntityPlayer player);
+	
+	/**
+	 * Has default handling if the Packet is received on the Client,
+	 * therefore this should be used if the Packet should just be receiveable by the Server.
+	 * @author JTK222
+	 *
+	 * @param <REQ>
+	 */
+	public static abstract class Server<REQ extends IMessage> extends PacketBase<REQ>{
+		@Override
+		@SideOnly(Side.CLIENT)
+		public void handleClientSide(REQ message, EntityPlayer player){
+			LogManager.getLogger().error("Received a packet that was ment to be only used to send Data form Client to Server!");
+			LogManager.getLogger().error("False Packet: "  + message.getClass().getSimpleName());
+		}
+	}
+	
+	/**
+	 * Has default handling if the Packet is received on the Server,
+	 * therefore this should be used if the Packet should just be receiveable by the Client.
+	 * @author JTK222
+	 *
+	 * @param <REQ>
+	 */
+	public static abstract class Client<REQ extends IMessage> extends PacketBase<REQ>{
+		@Override
+		public void handleServerSide(REQ message, EntityPlayer player){
+			LogManager.getLogger().error("Received a packet that was ment to be only used to send Data form Server to Client!");
+			LogManager.getLogger().error("False Packet: "  + message.getClass().getSimpleName());
+		}
+	}
+}
