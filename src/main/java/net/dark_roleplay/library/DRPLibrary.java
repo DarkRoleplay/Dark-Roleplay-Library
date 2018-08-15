d7cd91738affa23b8979aa4f0130f850a9dbc52d
JTK222 <lukasporwit3@googlemail.com>
2018-08-16 00:17:38 +0200

    Fixed a Server Crash

--- src/main/java/net/dark_roleplay/library/DRPLibrary.java
+++ src/main/java/net/dark_roleplay/library/DRPLibrary.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.dark_roleplay.library.experimental.guis.ModularGui_Handler;
+import net.dark_roleplay.library.sides.IProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
@@ -14,14 +15,20 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod(modid = References.MODID, version = References.VERSION, name = References.NAME, acceptedMinecraftVersions = References.ACCEPTEDVERSIONS, certificateFingerprint = "893c317856cf6819b3a8381c5664e4b06df7d1cc")
 public class DRPLibrary {
 
+	@SidedProxy(modId = References.MODID)
+	public static IProxy proxy;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){				
 		References.init(event);
@@ -29,12 +36,7 @@ public class DRPLibrary {
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		if(event.getSide().isClient()) {
-			IResourceManager manager = Minecraft.getMinecraft().getResourceManager();
-			if(manager instanceof IReloadableResourceManager) {
-			    ((IReloadableResourceManager)manager).registerReloadListener(new ModularGui_Handler());
-			}
-		}
+		proxy.init(event);
 	}
 	
 	@SubscribeEvent
@@ -68,6 +70,24 @@ public class DRPLibrary {
 			registry.register(block);
 			
 //			ItemBlock item = (ItemBlock) new ItemBlock(block).setRegistryName(block.getRegistryName()).setCreativeTab(tab);
+		}
+	}
+	
+	public static class ServerProxy implements IProxy{
+		
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static class ClientProxy implements IProxy{
+		
+		@Override
+		public void init(FMLInitializationEvent event) {
+			if(event.getSide().isClient()) {
+				IResourceManager manager = Minecraft.getMinecraft().getResourceManager();
+				if(manager instanceof IReloadableResourceManager) {
+				    ((IReloadableResourceManager)manager).registerReloadListener(new ModularGui_Handler());
+				}
+			}
 		}
 	}
 }
